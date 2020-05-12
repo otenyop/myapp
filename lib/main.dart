fbbae5ac486aa8af54f4499f0de9d2c5114d9c2b
Philip Otenyo <philip@Philips-MBP.home>
2020-05-12 23:00:40 +0300

    feature/initialCommit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 //This is a test app for flutter
 //This has been added in the development branch
 //Additional comment added
+//Added in the features/syncing branch
 import 'package:flutter/material.dart';
 
 void main(){
